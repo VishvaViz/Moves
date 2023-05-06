9f9d45a047f0e69f302ad28ddc959523f9ee2b4f
VishvaViz <vishvabulls@gmail.com>
2023-05-06 16:15:14 -0400

    secondcommit

--- src/Redux/message/message.reducer.js
+++
@@ -1,9 +0,0 @@
-let messageReducer=(state,action)=>{
-    switch(action.value){
-        case 'value':
-            return value
-        default:
-            return null
-    }
-}
-export{messageReducer}
--- src/Redux/store.js
+++
@@ -1,1 +0,0 @@
-let store=createStore(value)
